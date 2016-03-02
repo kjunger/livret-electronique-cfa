4aa6f6198440ee0b013de688a39bef7fba62ddfd
kjunger <kevin.junger@me.com>
2016-03-02 14:24:56 +0100

    Divers

--- _includes/head.php
+++ _includes/head.php
@@ -1,3 +1,4 @@
 <title>Livret Electronique de Suivi de l'Apprenti - Université de Rouen</title>
 <meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="_assets/css/normalize.css" />
+<link rel="stylesheet" type="text/css" href="_assets/css/_untouched/normalize.css" />
+<link rel="stylesheet" type="text/css" href="_assets/css/style.css" />
