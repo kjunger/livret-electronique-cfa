1e5e70f9878f9725732f80eb53ff7b45b4e59699
kjunger <kevin.junger@me.com>
2016-03-09 15:45:47 +0100

    Tests

--- _includes/head.php
+++ _includes/head.php
@@ -2,5 +2,5 @@
 <meta charset="utf-8" />
 <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css"  media="screen,projection" />
-<link type="text/css" rel="stylesheet" href="_asset/css/style.css" />
+<link type="text/css" rel="stylesheet" href="_assets/css/style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
