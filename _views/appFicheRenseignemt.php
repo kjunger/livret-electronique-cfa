f5019952033c24cd3e32de11aaf34bfd32b4f083
kjunger <kevin.junger@me.com>
2016-03-09 11:03:17 +0100

    Test

---
+++ _views/appFicheRenseignemt.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <form class="col s12">
+        <div class="row">
+            <div class="input-field col s6">
+                <input placeholder="Nom" id="nomApprenti" type="text" class="validate" />
+                <label for="nomApprenti">Nom</label>
+            </div>
+            <div class="input-field col s6">
+                <input placeholder="Nom" id="prenomApprenti" type="text" class="validate" />
+                <label for="prenomApprenti">Prénom</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s6">
+                <input placeholder="Date de naissance" id="dateNaissanceApprenti" type="text" class="validate" pattern="" />
+                <label for="dateNaissanceApprenti">Date de naissance - exemple : 20/02/1994</label>
+            </div>
+            <div class="input-field col s6">
+                <input placeholder="Lieu de naissance" id="lieuNaissanceApprenti" type="text" class="validate" />
+                <label for="lieuNaissanceApprenti">Né à</label>
+            </div>
+        </div>
+    </form>
+</div>
