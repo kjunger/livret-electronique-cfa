cedda6eb874b5cb37c4cc19b77671651d465a252
kjunger <kevin.junger@me.com>
2016-03-09 10:03:11 +0100

    Ajustements

--- _includes/head.php
+++ _includes/head.php
@@ -1,5 +1,2 @@
 <title>Livret Electronique de Suivi de l'Apprenti - Université de Rouen</title>
 <meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="_assets/css/_untouched/normalize.min.css" />
-<link rel="stylesheet" type="text/css" href="_assets/css/_untouched/font-awesome.min.css" />
-<link rel="stylesheet" type="text/css" href="_assets/css/style.css" />
