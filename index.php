50a562e2c9819da759d253f32793ea33959e7e2a
kjunger <kevin.junger@me.com>
2016-03-10 16:02:06 +0100

    Ajustements

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <?php include('_includes/head.php'); //Section HEAD du code HTML ?>
 </head><body><header>
 <?php include('_includes/header.php'); //Section HEADER du code HTML ?>
-</header><main class="row"><div class="col s3"><!-- Future sidebar de navigation --></div><div class="col s9">
+</header><main class="row"><div class="col s3">
+Future sidebar de navigation
+</div><div class="col s9">
 <?php include('_includes/controllers.php'); //Gestion des controlleurs pour l'affichage des pages du site ?>
 </div></main><footer><?php include('_includes/footer.php'); //Section FOOTER du code HTML ?>
 </footer></body>
