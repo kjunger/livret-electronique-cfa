1b565dc1449e07819aae84617bc924f2fa4ce163
kjunger <kevin.junger@me.com>
2016-03-10 10:52:10 +0100

    Ajustements

--- _views/appFicheRenseignemt.php
+++ _views/appFicheRenseignemt.php
@@ -49,7 +49,7 @@
                 <div class="row">
                     <div class="input-field col s12 m12 l4">
                         <i class="material-icons prefix">email</i>
-                        <input placeholder="Email" id="mailApprenti" type="email" class="validate" pattern="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])" required />
+                        <input placeholder="Email" id="mailApprenti" type="email" class="validate" required />
                         <label for="mailApprenti">Email</label>
                     </div>
                     <div class="input-field col s12 m12 l4">
@@ -144,7 +144,7 @@
                     <div class="row">
                         <div class="input-field col s12 m12 l4">
                             <i class="material-icons prefix">email</i>
-                            <input placeholder="Email" id="mailMaitreApp" type="mail" class="validate" pattern="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])" required />
+                            <input placeholder="Email" id="mailMaitreApp" type="mail" class="validate" required />
                             <label for="mailMaitreApp">Email</label>
                         </div>
                         <div class="input-field col s12 m12 l4">
