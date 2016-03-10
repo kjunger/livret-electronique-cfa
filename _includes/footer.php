23018d3d8e920bc5e84cc430e080397e56c6ac30
kjunger <kevin.junger@me.com>
2016-03-10 15:32:05 +0100

    Test footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,1 +1,7 @@
-<p>Copyright (c) 2016 - Université de Rouen</p>
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            &copy Université de Rouen - <?php echo date('y'); ?>
+        </div>
+    </div>
+</footer>
