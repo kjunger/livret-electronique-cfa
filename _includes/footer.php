d66c81ba5878dbf5618cdf126e9d7c75ec5bfd77
kjunger <kevin.junger@me.com>
2016-03-10 15:37:21 +0100

    Ajustements

--- _includes/footer.php
+++ _includes/footer.php
@@ -2,6 +2,8 @@
     <div class="footer-copyright">
         <div class="container">
             &copy Université de Rouen - <?php echo date('Y'); ?>
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
         </div>
     </div>
 </footer>
