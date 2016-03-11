c1299cd84905ff123ee0eefd41843887209a0948
kjunger <kevin.junger@me.com>
2016-03-11 11:50:34 +0100

    Correction des regex pour les input demandant des dates

--- _views/tutEntrevues.php
+++ _views/tutEntrevues.php
@@ -6,7 +6,7 @@
                 <span class="card-title">Entrevue</span>
                 <div class="row">
                     <div class="input-field col s12 m12 l12">
-                        <input placeholder="Date de l'entrevue" id="dateEntrevue" type="date" class="validate" required />
+                        <input placeholder="Date de l'entrevue" id="dateEntrevue" type="text" class="validate" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" required />
                         <label for="dateEntrevue">En date du</label>
                     </div>
                 </div>
