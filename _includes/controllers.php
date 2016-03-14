633e2270f350c5018d941bc71cebea7322427b80
Kevin JUNGER <kevin.junger@me.com>
2016-03-14 16:00:32 +0100

    Changement structure
    Simplification de la structure (réduction du nombre de controllers à 1 seul)

--- _includes/controllers.php
+++ _includes/controllers.php
@@ -1,11 +1,23 @@
-<?php if(!empty($_GET['control'])){
-    if(is_file('_controllers/'.$_GET['control'].'.php')){
-        include('_controllers/'.$_GET['control'].'.php');
+<nav class="grey lighten-5 blue-grey-text text-darken-4">
+    <div class="row">
+        <div class="col s12">
+            <a href="#!" class="breadcrumb blue-grey-text text-darken-4"><?php echo $_GET['cat']; ?></a>
+            <?php if(!empty($_GET['view'])){
+                echo '<a href="#!" class="breadcrumb blue-grey-text text-darken-4">'.$_GET['view'].'</a>';
+            } ?>
+        </div>
+    </div>
+</nav>
+<div class="section">
+<?php if(!empty($_GET['view'])){
+    if(is_file('_views/'.$_GET['cat'].'/'.$_GET['view'].'.php')){
+        include('_views/'.$_GET['cat'].'/'.$_GET['view'].'.php');
     }
     else{
         include('_controllers/404.php');
     }
 }
 else{
-    //include('_controllers/public.php');
-} ?>
+    include('_view/'.$_GET['cat']._root.php');
+}?>
+</div>
