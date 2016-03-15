a50c07eef60e771e76895661d154cd6c7a181d59
Kevin JUNGER <kevin.junger@me.com>
2016-03-15 16:41:30 +0100

    Ajustements

--- _assets/js/formAppInsertionPro.js
+++ _assets/js/formAppInsertionPro.js
@@ -2,8 +2,7 @@ $('document').ready(function () {
     $('#anneeProchaine').change(function () {
         var choixSelect = $('#anneeProchaine option:selected').val();
         if (choixSelect != "default") {
-            var choixInsert = "<?php include('_views/form/_includes/" + choixSelect + ".php'); ?>"
-            $('#choixAnneePro').html(choixInsert);
+            $('#choixAnneePro').load('/_views/form/_includes/' + choixSelect + '.php');
         }
     });
 });
