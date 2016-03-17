32f31f048f9fa787455e431397a5b3a3d6dd1bc4
Kevin JUNGER <kevin.junger@me.com>
2016-03-17 10:08:12 +0100

    Troisième essai d'intégration du CSS (ébauche)

--- _includes/header.php
+++ _includes/header.php
@@ -1,5 +1,11 @@
+<svg height="50" width="70" id="bouton">
+    <rect x="0" y="0" width="70" height="10" fill="white"></rect>
+    <rect x="0" y="18.75" width="70" height="10" fill="white"></rect>
+    <rect x="0" y="37.5" width="70" height="10" fill="white"></rect>
+</svg>
 <a href="index.php"><img src="_assets/img/header/logo.png" alt="Logo CFA-CFC" /></a>
-<p>Bienvenue, <br/><span id="nom">Prénom Nom</span></p>
+<p>Bienvenue,
+    <br/><span id="nom">Prénom Nom</span></p>
 <svg height="120" width="100" id="user">
     <circle cx="50" cy="60" r="40" stroke="grey" stroke-width="6" fill="white"></circle>
     <polygon points="50,50 70,98 30,98" fill="grey"></polygon>
