b72c71fb8901bb9cf7e52e49bdc668edae051b71
Kevin JUNGER <kevin.junger@me.com>
2016-03-17 12:16:13 +0100

    Ajustements (3)

--- _includes/head.php
+++ _includes/head.php
@@ -1,6 +1,6 @@
 <title>Livret Electronique de Suivi de l'Apprenti - Université de Rouen</title>
 <meta charset="utf-8" />
-<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+<!-- <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css"> -->
 <link type="text/css" rel="stylesheet" href="_assets/css/style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
