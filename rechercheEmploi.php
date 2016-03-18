474c5947aab825a510766f89da69b5fda95ec43b
Rémi Mathe <rem.mathe@gmail.com>
2016-03-18 10:46:37 +0100

    Mise en forme includes formulaires

---
+++ rechercheEmploi.php
@@ -0,0 +1,7 @@
+<p>Mobilité</p>
+<select name="mobilite" required>
+    <option selected disabled>Choisissez une option...</option>
+    <option value="locale">Locale</option>
+    <option value="regionale">Regionale</option>
+</select>
+<textarea id="souhaits"></textarea>
