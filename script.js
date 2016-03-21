fcbef0961caf4132e95a21653279c53d3ab7aa52
Rémi Mathe <rem.mathe@gmail.com>
2016-03-21 17:28:51 +0100

    Mise en place menu pour tablette/smartphone

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
 
+	$('#cssmenu li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp(200);
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown(200);
+			element.siblings('li').children('ul').slideUp(200);
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp(200);
+		}
+	});
+
 	var ecran = $(window).width();
 	if (ecran > 1000){
-		$('#cssmenu li.has-sub>a').on('click', function(){
-			$(this).removeAttr('href');
-			var element = $(this).parent('li');
-			if (element.hasClass('open')) {
-				element.removeClass('open');
-				element.find('li').removeClass('open');
-				element.find('ul').slideUp(200);
-			}
-			else {
-				element.addClass('open');
-				element.children('ul').slideDown(200);
-				element.siblings('li').children('ul').slideUp(200);
-				element.siblings('li').removeClass('open');
-				element.siblings('li').find('li').removeClass('open');
-				element.siblings('li').find('ul').slideUp(200);
-			}
-		});
 
 		$('header #user').on('click', function(){
 			var elem = $('header #deconnexion');
@@ -36,6 +37,152 @@ $(document).ready(function(){
 			}
 		});
 	}else{
+
+$('#cssmenu').css('display','none');
+$('#deconnexion-mobile').css('display','none');
+$('div.screen').css('right','0');
+$('div.screen').css('left','0');
+
+$('div.bouton').on('click', function(){
+			if( !$(this).hasClass('open') ){ 
+				openMenu(); 
+			}else { 
+				closeMenu(); 
+			}
+});
+
+$('div #cssmenu ul li a').on('click', function(e){
+		e.preventDefault();
+		closeMenu();
+});
+	
+function openMenu(){
+	
+	$('div.bouton').addClass('open');	
+    $('div.y').fadeOut(100);
+    $('div.screen').addClass('animate');
+    $('#cssmenu').css('display','');
+    $('div.screen').css('left','80%');	
+		
+	setTimeout(function(){					
+		$('div.x').addClass('rotate30'); 
+		$('div.z').addClass('rotate150'); 
+		setTimeout(function(){				
+			$('div.x').addClass('rotate45'); 
+			$('div.z').addClass('rotate135');  
+		}, 100);
+	}, 10);			
+}
+	
+function closeMenu(){
+	
+	$('div.screen').removeClass('animate');
+	$('div.y').fadeIn(150);
+	$('div.bouton').removeClass('open');	
+	$('div.x').removeClass('rotate45').addClass('rotate30'); 
+	$('div.z').removeClass('rotate135').addClass('rotate150');	
+	$('div.screen').css('left','0');
+	$('#cssmenu').css('display','none');			
+		
+	setTimeout(function(){ 			
+		$('div.x').removeClass('rotate30'); 
+		$('div.z').removeClass('rotate150'); 			
+	}, 50);
+	setTimeout(function(){				
+		$('div.x, div.z').removeClass('collapse');
+	}, 70);													
+		
+}
+
+$('header #user').on('click', function(){
+			if( !$(this).hasClass('open') ){ 
+				openMenuUser(); 
+			}else { 
+				closeMenuUser(); 
+			}
+});
+	
+function openMenuUser(){
+	$('header #user').addClass('open');	
+    $('div.screen').addClass('animate-user');
+    $('#deconnexion-mobile').css('display','');
+    $('div.screen').css('left','');	
+    $('div.screen').css('right','80%');	
+}
+	
+function closeMenuUser(){
+	
+	$('div.screen').removeClass('animate-user');
+	$('header #user').removeClass('open');
+	$('#deconnexion-mobile').css('display','none');
+	$('div.screen').css('right','80%');	
+	$('div.screen').css('left','0');	
+}
+
 	}
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
