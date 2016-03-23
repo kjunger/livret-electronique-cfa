d209fc545e3d03e41a13c6c71ca75c95b1e0c89e
Kevin JUNGER <kevin.junger@me.com>
2016-03-23 10:30:25 +0100

    Intégration CSS/logo page de connexion

--- _includes/login.php
+++ _includes/login.php
@@ -9,29 +9,28 @@
 </head>
 
 <body>
-    <div class="conteneur">
-        <div class="titre">
-            <h1>Livret Electronique de Suivi de l'Apprenti(e)</h1>
-        </div>
-        <div class="contenu">
-            <form method="post" action="../_scripts/scriptLogin.php">
-                <select name="situation" required>
-                    <option value="default" selected disabled>Sélectionnez votre situation</option>
-                    <option value="apprenti">Apprenti(e)</option>
-                    <option value="maitreapprentissage">Maître d'apprentissage</option>
-                    <option value="tuteurpedagogique">Tuteur pédagogique</option>
-                </select>
-                <input placeholder="Login" name="login" type="text" required />
-                <input placeholder="Mot de passe" name="mdp" type="password" required />
-                <p class="btn">
-                    <input type="submit" value="Valider" />
-                </p>
-            </form>
-            <div class="error"><?php 
+    <img src="../_assets/img/login/lesa.png" alt="logo" />
+    <div id="content">
+        <h2>Bienvenue, <span>veuillez vous connecter.</span></h2>
+        <form method="post" action="../_scripts/scriptLogin.php">
+            <select name="situation" required>
+                <option value="default" selected disabled>Sélectionnez votre situation</option>
+                <option value="apprenti">Apprenti(e)</option>
+                <option value="maitreapprentissage">Maître d'apprentissage</option>
+                <option value="tuteurpedagogique">Tuteur pédagogique</option>
+            </select>
+            <input placeholder="Login" name="login" type="text" required />
+            <input placeholder="Mot de passe" name="mdp" type="password" required />
+            <p class="btn">
+                <input type="submit" value="Valider" />
+            </p>
+        </form>
+        <div class="error">
+            <?php 
                 if (isset($_GET['error'])) {
                     echo "L'identification a échoué. Veuillez réessayer.";
                 }
-            ?></div>
+            ?>
         </div>
     </div>
 </body>
