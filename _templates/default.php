47d35c7df766765fb46f7fb91360077f6a1417f0
Kevin JUNGER <kevin.junger@me.com>
2016-03-23 11:52:30 +0100

    Simplication structure fichiers (work in progress)

---
+++ _templates/default.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Livret Electronique de Suivi de l'Apprenti - Université de Rouen</title>
+    <meta charset="utf-8" />
+    <link type="text/css" rel="stylesheet" href="_assets/css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script type="application/javascript" src="_assets/js/jquery-2.2.1.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="bouton">
+            <img src="_assets/icons/bouton.svg" alt="" />
+        </div>
+        <a href="index.php"><img src="_assets/img/header/logo.png" alt="Logo CFA-CFC" id="header-logo" /></a>
+        <p>Bienvenue,
+            <br/>
+            <?php
+                switch($_SESSION['situation']){
+                    case "apprenti":
+                        $name=$db->query("select prenomApprenti, nomApprenti from apprenti where loginApprenti='".$_SESSION['login']."';");
+                        $answer=$name->fetchAll();
+                        if(count($answer)==1){
+                            echo '<span id="nom">'.$answer[0]['prenomApprenti'].' '.$answer[0]['nomApprenti'];
+                        }
+                        break;
+                    case "maitreapprentissage":
+                        $name=$db->query("select prenomMaitreApprentissage, nomMaitreApprentissage from maitreapprentissage where loginMaitreApprentissage='".$_SESSION['login']."';");
+                        $answer=$name->fetchAll();
+                        if(count($answer)==1){
+                            echo '<span id="nom">'.$answer[0]['prenomMaitreApprentissage'].' '.$answer[0]['nomMaitreApprentissage'].'</span>';
+                        }
+                        break;
+                    case "tuteurpedagogique":
+                        $name=$db->query("select prenomTuteurPedagogique, nomTuteurPedagogique from tuteurpedagogique where loginTuteurPedagogique='".$_SESSION['login']."';");
+                        $answer=$name->fetchAll();
+                        if(count($answer)==1){
+                            echo '<span id="nom">'.$answer[0]['prenomTuteurPedagogique'].' '.$answer[0]['nomTuteurPedagogique'].'</span>';
+                        }
+                        break;
+                }
+            ?></p>
+        <div id="user">
+            <img src="_assets/icons/user.svg" alt="" />
+        </div>
+        <div id="deconnexion" class="">
+            <ul class="hidden">
+                <li>
+                    <a href=""><img src="_assets/icons/form.svg" alt="" />Profil</a>
+                </li>
+                <li class="separateur">-</li>
+                <li>
+                    <a href="_scripts/scriptLogout.php"><img src="_assets/icons/form.svg" alt="" />Deconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <nav id="cssmenu">
+        <ul>
+            <li>
+                <a href="index.php" id="actif"><img src="_assets/icons/accueil.svg" alt="Accueil" class="icn" />Accueil</a>
+            </li>
+            <li>
+                <a href="index.php?cat=eval"><img src="_assets/icons/eval.svg" alt="Evaluation" class="icn" />Evaluation</a>
+            </li>
+            <li class="has-sub">
+                <a href="#"><img src="_assets/icons/form.svg" alt="Formulaires" class="icn" />Formulaires</a>
+                <ul>
+                    <li><a href="index.php?cat=form&view=appFicheRenseignemt">Fiche de renseignement</a></li>
+                    <li><a href="index.php?cat=form&view=appInsertionPro">Insertion professionnelle et suivi des diplômés</a></li>
+                    <li><a href="index.php?cat=form&view=tutEntrevues">Entrevues</a></li>
+                    <li><a href="index.php?cat=form&view=tutVisiteEnt">Visites en entreprise</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="index.php?cat=contrat"><img src="_assets/icons/contrat.svg" alt="Contrat" class="icn" />Contrat</a>
+            </li>
+            <li>
+                <a href="index.php?cat=fichiers"><img src="_assets/icons/fichiers.svg" alt="Fichiers" class="icn" />Fichiers</a>
+            </li>
+            <li>
+                <a href="index.php?cat=contacts"><img src="_assets/icons/contacts.svg" alt="Contacts" class="icn" />Contacts</a>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <p id="breadcrumbs">
+            <?php 
+                if (!empty($_GET['cat'])) {
+                    echo '<a href="index.php?cat='.$_GET['cat'].'">'.$_GET['cat'].'</a> > ';
+                    if (!empty($_GET['view'])) {
+                        echo '<a href="index.php?cat='.$_GET['cat'].'&view='.$_GET['view'].'">'.$_GET['view'].'</a>';
+                    }
+                } else {
+                    echo '<a href="index.php">Accueil</a> > ';
+                } 
+            ?>
+        </p>
+        <?php 
+            if(!empty($_GET['cat']) && !empty($_GET['view'])){
+                if(is_file('_views/'.$_GET['cat'].'/'.$_GET['view'].'.php')){
+                    include('_views/'.$_GET['cat'].'/'.$_GET['view'].'.php');
+                }
+                else{
+                    include('_views/404.php');
+                }
+            }
+            else{
+                include('_includes/accueil.php');
+            }
+        ?>
+    </main>
+    <footer>
+        <img src="_assets/img/footer/logo_univ_rouen.png" alt="Logo de l'Université de Rouen" id="logo_univ" />
+        <img src="_assets/img/footer/logo_iut_rouen.png" alt="Logo de l'IUT de Rouen" id="logo_etab" />
+        <ul>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Mentions légales</a></li>
+        </ul>
+        <img src="_assets/img/footer/logo_region.png" alt="Logo de la région" id="logo_region" />
+        <img src="_assets/img/footer/logo_ministere.png" alt="Logo du Ministère de l'Enseignement Supérieur et de la Recherche" id="logo_ministere" />
+    </footer>
+</body>
+<script type="application/javascript" src="_assets/js/btnConnexion.js"></script>
+</html>
--- index.php
+++ index.php
@@ -4,30 +4,6 @@
     if(empty($_SESSION['login']) && empty($_SESSION['situation'])):
         header('Location:_includes/login.php');
     else:
+        include('_templates/default.php');
+    endif; 
 ?>
-    <!DOCTYPE html>
-    <html lang="fr">
-
-    <head>
-        <?php include('_includes/head.php'); ?>
-            <?php include('_includes/scriptsBefore.php'); ?>
-    </head>
-
-    <body>
-        <header>
-            <?php include('_includes/header.php'); ?>
-        </header>
-        <nav id="cssmenu">
-            <?php include('_includes/sidebar.php'); ?>
-        </nav>
-        <main>
-            <?php include('_includes/controllers.php'); ?>
-        </main>
-        <footer>
-            <?php include('_includes/footer.php'); ?>
-        </footer>
-    </body>
-    <?php include('_includes/scriptsAfter.php'); ?>
-
-    </html>
-<?php endif; ?>
