76479e1824bcabfd236b3097128f8b914cce04f1
Kevin JUNGER <kevin.junger@me.com>
2016-04-28 16:54:48 +0200

    Suite test export PDF formulaire

--- _views/includes/private/form/fiche_renseignements.php
+++ _views/includes/private/form/fiche_renseignements.php
@@ -12,28 +12,28 @@
                 <input placeholder="Prénom" name="prenomApprenti" type="text" value="<?php echo $userInfo['user']['prenom']; ?>" required />
             </p>
             <p>
-                <input placeholder="Date de naissance (XX/XX/XXXX)" id="dateNaissanceApprenti" type="text" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" required />
+                <input placeholder="Date de naissance (jj/mm/aaaa)" name="dateNaissanceApprenti" type="text" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" required />
             </p>
             <p>
-                <input placeholder="Lieu de naissance" id="lieuNaissanceApprenti" type="text" required />
+                <input placeholder="Lieu de naissance" name="lieuNaissanceApprenti" type="text" required />
             </p>
             <p>
-                <input placeholder="Adresse" id="adresseApprenti" type="text" value="<?php echo $userInfo['user']['adresse']; ?>" required />
+                <input placeholder="Adresse" name="adresseApprenti" type="text" value="<?php echo $userInfo['user']['adresse']; ?>" required />
             </p>
             <p>
-                <input placeholder="Code postal" id="cpApprenti" type="text" pattern="[0-9]{5}" value="<?php echo $userInfo['user']['cp']; ?>" required />
+                <input placeholder="Code postal" name="cpApprenti" type="text" pattern="[0-9]{5}" value="<?php echo $userInfo['user']['cp']; ?>" required />
             </p>
             <p>
-                <input placeholder="Ville" id="villeApprenti" type="text" value="<?php echo $userInfo['user']['ville']; ?>" required />
+                <input placeholder="Ville" name="villeApprenti" type="text" value="<?php echo $userInfo['user']['ville']; ?>" required />
             </p>
             <p>
-                <input placeholder="Email" id="mailApprenti" type="email" value="<?php echo $userInfo['user']['email']; ?>" required />
+                <input placeholder="Email" name="mailApprenti" type="email" value="<?php echo $userInfo['user']['email']; ?>" required />
             </p>
             <p>
-                <input placeholder="Téléphone" id="telApprenti" type="tel" pattern="0[0-9]{9}" value="<?php echo $userInfo['user']['tel']; ?>" required />
+                <input placeholder="Téléphone" name="telApprenti" type="tel" pattern="0[0-9]{9}" value="<?php echo $userInfo['user']['tel']; ?>" required />
             </p>
             <p>
-                <input placeholder="Portable" id="portApprenti" type="tel" pattern="0[6-7]{1}[0-9]{8}" value="<?php echo $userInfo['user']['port']; ?>" required />
+                <input placeholder="Portable" name="portApprenti" type="tel" pattern="0[6-7]{1}[0-9]{8}" value="<?php echo $userInfo['user']['port']; ?>" required />
             </p>
         </div>
     </div>
@@ -43,17 +43,17 @@
         </div>
         <div class="contenu">
             <p>
-                <input placeholder="Raison sociale" id="raisonSocialeEnt" type="text" required />
+                <input placeholder="Raison sociale" id="raisonSocialeEnt" type="text" />
             </p>
             <h2>Siège social</h2>
             <p>
-                <input placeholder="Adresse du siège social" id="adSiege" type="text" required />
+                <input placeholder="Adresse du siège social" id="adSiege" type="text" />
             </p>
             <p>
-                <input placeholder="Code postal" id="cpSiege" type="text" pattern="[0-9]{5}" required />
+                <input placeholder="Code postal" id="cpSiege" type="text" pattern="[0-9]{5}" />
             </p>
             <p>
-                <input placeholder="Ville" id="villeSiege" type="text" required />
+                <input placeholder="Ville" id="villeSiege" type="text" />
             </p>
             <h2>Site d'accueil de l'apprenti(e) <span class="info">(si différent du siège social)</span></h2>
             <p>
@@ -67,19 +67,19 @@
             </p>
             <h2>Maître d'apprentissage</h2>
             <p>
-                <input placeholder="Nom" id="nomMaitreApp" type="text" required />
+                <input placeholder="Nom" id="nomMaitreApp" type="text" />
             </p>
             <p>
-                <input placeholder="Prénom" id="prenomMaitreApp" type="text" required />
+                <input placeholder="Prénom" id="prenomMaitreApp" type="text" />
             </p>
             <p>
-                <input placeholder="Fonction" id="fonctionMaitreApp" type="text" required />
+                <input placeholder="Fonction" id="fonctionMaitreApp" type="text" />
             </p>
             <p>
-                <input placeholder="Email" id="mailMaitreApp" type="email" required />
+                <input placeholder="Email" id="mailMaitreApp" type="email" />
             </p>
             <p>
-                <input placeholder="Téléphone" id="telMaitreApp" type="tel" pattern="0[0-9]{9}" required />
+                <input placeholder="Téléphone" id="telMaitreApp" type="tel" pattern="0[0-9]{9}" />
             </p>
             <p>
                 <input placeholder="Portable" id="portMaitreApp" type="tel" pattern="0[6-7]{1}[0-9]{8}" />
