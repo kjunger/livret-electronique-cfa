9d8c439e8d99b418f34a59fab76e24b47f273c10
Rémi Mathe <rem.mathe@gmail.com>
2016-04-28 15:25:21 +0200

    Modification formulaire

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ $(document).ready(function(){
 $('.titre').append('<svg height:"25" width:"25" class=""><polygon points="0,0 25,12.5 0,25" fill="#363636" stroke="none" /></svg>');
 $('.conteneur').addClass('open');
 sousmenu();
+sousliens();
 menu();
 menu_user();
 affichage_deconnexion();
 redimensionnement();
 contenu_mobile();
+appInput();
+appSubmit();
 $(window).resize(redimensionnement);
 
 //Variables
@@ -17,10 +20,10 @@ var hauteurEcran = $(window).height();
 
 //Fonctions
 function redimensionnement_main(){
-	var hauteurMain = $('#main').height();
+	var hauteurMain = $('#main').outerHeight();
 	var hauteurHeader = $('header').height();
 	var hauteurFooter = $('footer').height();
-	var largeurMain = $('#main').width();
+	var largeurMain = $('#main').outerWidth();
 	var largeurMenu = $('#cssmenu').outerWidth() + 3;
 	hauteurMain = hauteurMain - hauteurFooter - hauteurHeader;
 	largeurMain = largeurMain - largeurMenu;
@@ -46,7 +49,7 @@ function redimensionnement_menu_user(){
 }
 
 function redimensionnement_main_mobile(){
-	var hauteurMain = $('#main').height();
+	var hauteurMain = $('#main').outerHeight();
 	var hauteurHeader = $('header').height();
 	hauteurMain = hauteurMain - hauteurHeader;
 	$('#main').css('height', hauteurMain);
@@ -78,7 +81,6 @@ function affichage_deconnexion(){
 }
 
 function openMenu(){
-	
 	$('div.bouton').addClass('open');	
     $('div.y').fadeOut(100);
     $('div.screen').addClass('animate');
@@ -93,11 +95,10 @@ function openMenu(){
 			$('div.x').addClass('rotate45'); 
 			$('div.z').addClass('rotate135');  
 		}, 100);
-	}, 10);			
+	}, 10);	
 }
 	
 function closeMenu(){
-	
 	$('div.screen').removeClass('animate');
 	$('div.y').fadeIn(150);
 	$('div.bouton').removeClass('open');	
@@ -154,10 +155,32 @@ function menu_user(){
 }
 
 function sousmenu(){
+	$('#cssmenu ul ul').css('display', 'none');
 	$('#cssmenu li.has-sub>a').on('click', function(){
 		$(this).removeAttr('href');
 		var element = $(this).parent('li');
 		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').stop().slideUp(200);
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').stop().slideDown(200);
+			element.siblings('li').children('ul').slideUp(200);
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp(200);
+		}
+	});
+}
+
+function sousliens(){
+	$('.sous-ul-liens').css('display', 'none');
+	$('.lien-has-sub').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if(element.hasClass('open')){
 			element.removeClass('open');
 			element.find('li').removeClass('open');
 			element.find('ul').stop().slideUp(200);
@@ -213,5 +236,71 @@ function contenu_mobile(){
 		
 	});
 }
+
+function appInput(){
+	var input = $('input[type="text"], input[type="email"], input[type="tel"], input[type="password"], textarea');
+	input.addClass('input-field');
+	$(input).wrap('<span class="input">');
+	$(input).after('<label class="input-label"><span class="input-label-content">');
+	$('textarea').addClass('textarea');
+	if($('input').hasClass('input-field')){
+		$(input).each(function(){
+			var placeholder = $(this).attr('placeholder');
+			var ident = $(this).attr('id');
+			$(this).next('.input-label').attr('for', ident);
+			$(this).next('.input-label').children('.input-label-content').prepend(placeholder);
+			$(this).attr('placeholder', '');
+		});
+	}
+}
+
+function appSubmit(){
+	var submit = $('input[type="submit"');
+	submit.addClass('submit-field');
+	$(submit).wrap('<span class="submit">');
+	$(submit).after('<label class="submit-label"><span class="submit-label-content">');
+	$(submit).next('.submit-label').children('.submit-label-content').prepend($(submit).val());
+}
 //
+
+$('#main').on('click', function(){
+	if($('#bouton-menu-user').hasClass('open')){
+		closeMenuUser();
+	}
+	if($('div.bouton').hasClass('open')){
+		closeMenu();
+	}
+});
+
+(function() {
+	if (!String.prototype.trim) {
+		(function() {
+			var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+			String.prototype.trim = function() {
+				return this.replace(rtrim, '');
+			};
+		})();
+	}
+
+	[].slice.call( document.querySelectorAll( '.input-field' ) ).forEach( function( inputEl ) {
+		// in case the input is already filled..
+		if( inputEl.value.trim() !== '' ) {
+			classie.add( inputEl.parentNode, 'input--filled' );
+		}
+
+		// events:
+		inputEl.addEventListener( 'focus', onInputFocus );
+		inputEl.addEventListener( 'blur', onInputBlur );
+	} );
+
+	function onInputFocus( ev ) {
+		classie.add( ev.target.parentNode, 'input--filled' );
+	}
+	function onInputBlur( ev ) {
+		if( ev.target.value.trim() === '' ) {
+			classie.remove( ev.target.parentNode, 'input--filled' );
+		}
+	}
+})();
+
 });
